69ad61a456873056d23316f7e2e91ffe48f15787
jas8157 <jas8157@naver.com>
2023-12-21 16:48:49 +0900

    app css 세로영역 수정

--- src/page/MainHomePage.js
+++ src/page/MainHomePage.js
@@ -4,17 +4,13 @@ import HomeBrandPage from "../component/HomeBrandPage";
 import HomeBestProduct from "../component/HomeBestProduct";
 import HomeEvent from "../component/HomeEvent";
 import "../style/event.css"
+
 const MainHomePage = () => {
   return (
     <div className="home">
       
       <SlideBanner />
       <HomeBrandPage />
-      
-      {/* <div className='home-banner'>
-        <div className='home-banner-l'>1번</div>
-        <div className='home-banner-r'>2번</div>
-      </div> */}
       <HomeBestProduct />
       <HomeEvent />
     </div>
