6eeb42c5483e95f3d64b4d3560f2135d9165d6bd
jas8157 <jas8157@naver.com>
2023-12-21 17:39:59 +0900

    store, event card css 수정

--- src/utils/api.js
+++ src/utils/api.js
@@ -4,10 +4,6 @@ import axios from "axios";
 const PROXY_URL = process.env.REACT_APP_BACKEND_PROXY
 const LOCAL_URL = process.env.REACT_APP_LOCAL_BACKEND
 const BASE_URL = process.env.NODE_ENV === "production" ?  PROXY_URL : LOCAL_URL;
-console.log("1번", PROXY_URL)
-console.log("2번", LOCAL_URL)
-console.log("3번", BASE_URL)
-// const BASE_URL = process.env.REACT_APP_BACKEND_PROXY;
 
 // 프론트를 프론트로 back은 public
 const api = axios.create({
